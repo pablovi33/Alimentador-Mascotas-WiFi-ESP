e37a8ef505b72ddc1ccc7ee9e8df551a4552077d
Pablo Vivar Colina <pablovc3@multivac.www.ditac.org>
2020-09-10 17:38:03 -0500

    se tradujeron los comentarios y modificaron cnflicots con variables del ssid y password

--- AlimentadorMascotasWiFi.ino
+++ AlimentadorMascotasWiFi.ino
@@ -13,15 +13,12 @@
  -ESP8266 como cliente de servicio para la hora UTC
  -ESP8266 como servidor para tener acciOn manual del motor a pasos
  -movimiento de motor a pasos con biblioteca especializada
-*/
-
-/*
     This sketch demonstrates how to set up a simple HTTP-like server.
-    The server will set a GPIO pin depending on the request
-      http://server_ip/gpio/0 will set the GPIO2 low,
-      http://server_ip/gpio/1 will set the GPIO2 high
-    server_ip is the IP address of the ESP8266 module, will be
-    printed to Serial when the module is connected.
+    El servidor realizarA una accion dependiendo de la peticiOn
+      http://server_ip/gpio/1 rotara el motor a pasos CW,
+      http://server_ip/gpio/2 rotara el motor a pasos CCW
+    server_ip es la direcciOn IP del mOdulo ESP8266 
+    se imprimirA en el monitor serial al inicar conxiOn a Internet.
 
 //Mapeo de pines para plca de desarrollo ESP8266
 PIN       Arduino IDE
@@ -47,12 +44,10 @@ D2           -> digital 16
 */
 
 #include <ESP8266WiFi.h>
+//SE DEBE AGREGAR A PREFERENCIAS https://arduino.esp8266.com/stable/package_esp8266com_index.json
+//#define STASSID "your-ssid"
+//#define STAPSK  "your-password"
 
-#ifndef STASSID
-#define STASSID "your-ssid" //  your network SSID (name)
-#define STAPSK  "your-password" // your network password
-
-#endif
 #include <Stepper.h>
 #include "RTClib.h" // Date and time functions using a DS1307 RTC connected via I2C and Wire lib
 
@@ -63,45 +58,38 @@ D2           -> digital 16
  ********************/
 #include <WiFiUdp.h>
 
+
+
 unsigned int localPort = 2390;      // local port to listen for UDP packets
 int hora, minuto, segundo, horaUTC, minutoUTC, segundoUTC, a,rota2;
 
-/* Don't hardwire the IP address or we won't get the benefits of the pool.
- *  Lookup the IP address for the host name instead */
+/* No ingresar de manera directa la direcciOn IP o no se obtendrán los beneficios de acciOn de pool.
+ *  Busque mejor la direcciOn IP */
 //IPAddress timeServer(129, 6, 15, 28); // time.nist.gov NTP server
 IPAddress timeServerIP; // time.nist.gov NTP server address
 const char* ntpServerName = "time.nist.gov";
 
-const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
+const int NTP_PACKET_SIZE = 48; // la estampa tiempo NTP son los primeros 48 bytes del mensaje
 
-byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
+byte packetBuffer[ NTP_PACKET_SIZE]; //buffer para mantener los paquetes salientes
 
-// A UDP instance to let us send and receive packets over UDP
+// una instancia UDP nos permite enviar y recibir paquetes sobre UDP
 WiFiUDP udp;
 
-//const int stepsPerRevolution = 341;  //30 grados
 
 const int stepsPerRevolution = 344;  //30 grados V2 funciona a la perfección
 
 
-// for your motor
-
-// initialize the stepper library on pins 8 through 11:
-//Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
-//Stepper myStepper(stepsPerRevolution, 13, 14, 12, 4);
 Stepper myStepper(stepsPerRevolution, 13, 14, 12, 16);
 
 
-const char* ssid = STASSID;
-const char* password = STAPSK;
-
-// Create an instance of the server
-// specify the port to listen on as an argument
+// Crea una instancia de servidor
+// especifica que puerto excuchar como argumento
 WiFiServer server(80);
 
 RTC_DS1307 rtc;
 
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+char diasSemana[7][12] = {"Sabado", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"};
 
 void setup () {
   //Serial.begin(57600);(ver conflicto con tasa de baudio)
@@ -119,11 +107,14 @@ Serial.begin(115200);
   // Connect to WiFi network
   Serial.println();
   Serial.println();
-  Serial.print(F("Connecting to "));
-  Serial.println(ssid);
+  //Serial.print(F("Connecting to "));
+  Serial.println(STASSID);
+  //Serial.println(ssid);
+    WiFi.mode(WIFI_STA);
+  //WiFi.begin(ssid, pass);
 
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
+
+  WiFi.begin(STASSID,STAPSK);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -140,7 +131,7 @@ Serial.begin(115200);
   //Rutina de cliente
   Serial.println("Starting UDP");
   udp.begin(localPort);
-  Serial.print("Local port: ");
+  Serial.print("Puerto local: ");
   Serial.println(udp.localPort());
   
 
@@ -148,47 +139,48 @@ Serial.begin(115200);
 
 //rutina de sincronizacion WiFi
 
-  //get a random server from the pool
+  //obtener servidor aleatorio de pool
     WiFi.hostByName(ntpServerName, timeServerIP); 
   
-    sendNTPpacket(timeServerIP); // send an NTP packet to a time server
-    // wait to see if a reply is available
+    sendNTPpacket(timeServerIP); // enviar un paquete NTP a un servidor de tiempo
+    // esperar si una respuesta estA disponible
     delay(1000);
     
     int cb = udp.parsePacket();
     if (!cb) {
-      Serial.println("no packet yet");
+      Serial.println("no hay paquete todavia");
     }
     else {
-      Serial.print("packet received, length=");
+      Serial.print("paquete recibido, largo=");
       Serial.println(cb);
-      // We've received a packet, read the data from it
-      udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+      //Hemos recibido el paquete, leemos los datos de El
+      udp.read(packetBuffer, NTP_PACKET_SIZE); // leer paquete hacia el buffer
   
-      //the timestamp starts at byte 40 of the received packet and is four bytes,
-      // or two words, long. First, esxtract the two words:
+      //la estampa de tiempo empieza en el byte 40 de los paquetes recibidos y es de 4 bytes,
+      // o dos palabras de largo. Primero, se extraen dos palabras:
   
       unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
       unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
-      // combine the four bytes (two words) into a long integer
-      // this is NTP time (seconds since Jan 1 1900):
+      // combina los cuatro bytes (dos palabras) en un entero long
+      // este es el NTP de tiempo (segundos desde 1 Enero 1900):
       unsigned long secsSince1900 = highWord << 16 | lowWord;
-      Serial.print("Seconds since Jan 1 1900 = " );
+      Serial.print("Segundos desde 1 Enero 1900 = " );
       Serial.println(secsSince1900);
   
-      // now convert NTP time into everyday time:
-      Serial.print("Unix time = ");
-      // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+      // ahora convertimos el tiempo NTP en tiempo diario:
+      Serial.print("tiempo Unix=");
+      // El tiempo Unix empieza en 1 Enero  1970. en seconds, eso es 2208988800:
       const unsigned long seventyYears = 2208988800UL;
-      // subtract seventy years:
+      // restamos setenta anyos:
       unsigned long epoch = secsSince1900 - seventyYears;
-      // print Unix time:
+      // imprimos tiempo Unix:
       Serial.println(epoch);
   
   
-      // print the hour, minute and second:
-      Serial.print("The UTC-5 time is ");       // UTC is the time at Greenwich Meridian (GMT)
-      horaUTC=((epoch  % 86400L) / 3600-5);
+      // imprimir la hor minuto y segundo:
+      Serial.print("El tiempo UTC-5 es");       // UTC es el tiempo en el meridiano de Greenwich (GMT)
+      horaUTC=((epoch  % 86400L) / 3600-5);//para la hora central de Mexico se necesita restar 5 horas al tiempo coordinado universal
+      //para ello tambiEn es necesario aislar los casos para cuando la restas dan nUmeros negativos y traducirlas a un fromato legible
       if(horaUTC==-1){
         horaUTC=23;
       }else if(horaUTC==-2){
@@ -200,63 +192,57 @@ Serial.begin(115200);
       }else if(horaUTC==-5){
         horaUTC=19;
       }
-      Serial.print(horaUTC); // print the hour (86400 equals secs per day)
+      
+      Serial.print(horaUTC); // imprime la hora (equivale a 86400 segundos a diario)
       Serial.print(':');
       minutoUTC=(epoch % 3600) / 60;
       if ( minutoUTC < 10 ) {
-        // In the first 10 minutes of each hour, we'll want a leading '0'
+        //En los primeros 10 minutos de cada hora, nosotros querremos que se conduzca a '0'
         Serial.print('0');
         minutoUTC=0;
       }
-      Serial.print(minutoUTC); // print the minute (3600 equals secs per minute)
+      Serial.print(minutoUTC); // imprimimos minutos (3600 equivale a segundos por minuto)
       Serial.print(':');
       segundoUTC=epoch % 60;
       if ( segundoUTC < 10 ) {
-        // In the first 10 seconds of each minute, we'll want a leading '0'
+        // En los primeros 10 segundos de cada minutos querremos que se conduzca a '0'
         Serial.print('0');
         segundoUTC=0;
       }
-      Serial.println(segundoUTC); // print the second
+      Serial.println(segundoUTC); // imprimimos segundos
     }
     //esperar 3 segundos
     delay(3000);
 
   // comienza el servidor
   server.begin();
-  Serial.println(F("Server started"));
+  Serial.println(F("Inicia Servidos"));
 
 
   if (! rtc.begin()) {
-    Serial.println("Couldn't find RTC");
+    Serial.println("No se ha encontrado RTC");
     Serial.flush();
     abort();
   }
 
   if (! rtc.isrunning()) {
-    Serial.println("RTC is NOT running, let's set the time!");
-    // When time needs to be set on a new device, or after a power loss, the
-    // following line sets the RTC to the date & time this sketch was compiled
+    Serial.println("RTC no esta activo, ¡configuremos el tiempo!");
+    // Cuando el tiempo necesita ser configurado en un nuevo dispositivo, o despues de un bajo de corriente, la
+    // siguiente línea colocarA el RTC a la fecha y tiempo en el que este programa ha sido compilado
     //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    rtc.adjust(DateTime(2020,9,7,horaUTC,minutoUTC,segundoUTC));
-    // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
+    rtc.adjust(DateTime(2020,9,7,horaUTC,minutoUTC,segundoUTC));//para este caso hemos cargado la hora consultada en internet al mOdulo RTC
+    // Esta linea configura el RTC con una fecha y tiempo en especIfico como se muestra en el ejemplot
+    // Enero 21, 2014 tiempo lo llamarIamos asI:
      //rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
     // rtc.adjust(DateTime(2020, 9, 7, 11, 00, 0));
   }
 
-  // When time needs to be re-set on a previously configured device, the
-  // following line sets the RTC to the date & time this sketch was compiled
-   //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-   rtc.adjust(DateTime(2020,9,7,horaUTC,minutoUTC,segundoUTC));
-  // This line sets the RTC with an explicit date & time, for example to set
-  // January 21, 2014 at 3am you would call:
-  // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
- // rtc.adjust(DateTime(2020, 9, 7, 11, 00, 0));  
-
+  //Falta funcion que actualice fecha automaticamente
+   rtc.adjust(DateTime(2020,9,10,horaUTC,minutoUTC,segundoUTC));//para este caso hemos cargado la hora consultada en internet al mOdulo RTC
   
 }//fin setup
 
-//funcion de paro motor a pasos
+//funcion de PARO motor a pasos
 void detener(){
   digitalWrite(13,LOW);
   digitalWrite(14,LOW);
@@ -265,27 +251,27 @@ void detener(){
 }
 
 //funcion peticion de hora a servidor web
-// send an NTP request to the time server at the given address
+// enviar una peticiOn NTP a un servidor de tiempo dada una direcciOn
 unsigned long sendNTPpacket(IPAddress& address)
 {
-  Serial.println("sending NTP packet...");
-  // set all bytes in the buffer to 0
+  Serial.println("enviando paquete NTP ...");
+  // configurar todos los bytes en el buffer a '0'
   memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
+  // Inizializa valores requeridos de la forma de peticiOn NTP
+  // (ver URL arriba para los detalles de los paquetes)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Modo
+  packetBuffer[1] = 0;     // Stratum, o tipo de reloj
+  packetBuffer[2] = 6;     // Intervalo de poleo (Polling)
+  packetBuffer[3] = 0xEC;  // Peer Reloj Precision
+  // 8 bytes de zero para Root Delay & Root Dispersion
   packetBuffer[12]  = 49;
   packetBuffer[13]  = 0x4E;
   packetBuffer[14]  = 49;
   packetBuffer[15]  = 52;
 
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:
-  udp.beginPacket(address, 123); //NTP requests are to port 123
+  // todos los campos NTP tienen valores dados, ahora
+  // es posible enviar paquetes con una peticion de estampa de tiempo(timestamp):
+  udp.beginPacket(address, 123); //peticiOn NTP son el puerto 123
   udp.write(packetBuffer, NTP_PACKET_SIZE);
   udp.endPacket();
 }
@@ -302,7 +288,7 @@ void loop () {
     Serial.print('/');
     Serial.print(now.day(), DEC);
     Serial.print(" (");
-    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(diasSemana[now.dayOfTheWeek()]);
     Serial.print(") ");
     Serial.print(now.hour(), DEC);
     Serial.print(':');
@@ -312,7 +298,6 @@ void loop () {
     Serial.print(now.second(), DEC);
     Serial.println();
 
- 
     //Para el uso del aparato se tiene un espacio de 6:30 a 22:30, lo que da un total de 16 horas
     //en espacios equitativos se tiene que cada intervalo debe ser de 2 horas con 42 min
 
@@ -385,21 +370,21 @@ void loop () {
     delay(3000);
     //fin rutina de RTC
 
-    // Check if a client has connected
+    // Revisar si un cliente se ha conectado
   WiFiClient client = server.available();
   if (!client) {
     return;
   }
-  Serial.println(F("new client"));
+  Serial.println(F("nuevo cliente"));
 
-  client.setTimeout(5000); // default is 1000
+  client.setTimeout(5000); // predeterminado es 1000
 
-  // Read the first line of the request
+  // Leer la primera linea de la peticiOn
   String req = client.readStringUntil('\r');
-  Serial.println(F("request: "));
+  Serial.println(F("peticiOn: "));
   Serial.println(req);
 
-  // Match the request
+  // Enlazar las peticiones
   int val;
   if (req.indexOf(F("/gpio/1")) != -1) {
     val = 1;
@@ -415,10 +400,7 @@ void loop () {
     val = digitalRead(LED_BUILTIN);
   }
 
-
- rota2=stepsPerRevolution*6;
-  // Set LED according to the request
-  //digitalWrite(LED_BUILTIN, val);
+  //realizar accion de acuerdo a la peticiOn
 
   if(val==1){
     Serial.println("Giro CLW");
@@ -429,7 +411,7 @@ void loop () {
     myStepper.step(-stepsPerRevolution);
     detener();
   }
-  
+//Accion extra no utililzada  
   /*else if(val==3){
      Serial.println("Giro FULL CCLW");
     myStepper.step(rota2);
@@ -439,16 +421,16 @@ void loop () {
     detener();
   }
   
-  // read/ignore the rest of the request
+  // leer/ignorar el resto de la peticiOn
   // do not client.flush(): it is for output only, see below
   while (client.available()) {
-    // byte by byte is not very efficient
+    // byte por byte no es muy eficiente
     client.read();
   }
 
-  // Send the response to the client
-  // it is OK for multiple small client.print/write,
-  // because nagle algorithm will group them into one single packet
+  //Enviar la respuesta al cliente
+  // estA BIEN si es para pequenyos multiples client.print/write,
+  // porque el algoritmo de nagle los agruparA en un paquete sencillo
   client.print(F("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\n Hora "));
   //client.print((val) ? F("detenido") : F("moviendose"));
   client.print(hora);
@@ -465,9 +447,9 @@ void loop () {
   //client.print(WiFi.localIP());
   //client.print(F("/gpio/3'>aqui</a> para rotacion Completa.</html>"));
 
-  // The client will actually be *flushed* then disconnected
-  // when the function returns and 'client' object is destroyed (out-of-scope)
-  // flush = ensure written data are received by the other side
+  // El cliente será descargado (*flushed*) y luego desconectado
+  // cuando la funcion regresa y el objeto 'client' es destruido fuera de observaciOn (out-of-scope)
+  // flush = asegura que los datos escritos son recibidos del otro lado
   Serial.println(F("Desconectando del cliente"));
 
     
